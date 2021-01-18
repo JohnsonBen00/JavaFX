4e37f7141cc57d37e0f7bd590d49752adc687bbc
Benhur Johnson Tadiparti <42557348+JohnsonBen00@users.noreply.github.com>
2021-01-18 14:40:43 -0800

    Add files via upload

---
+++ csc335-lab1-JohnsonBen00-master/src/Lab1.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Benhur Johnson Tadiparti
+ * 
+ * 
+ */
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// DONE Change this to "Hello CSC 335, I'm YOURNAME"
+		System.out.println("Hello CSC 335, I'm Benhur Johnson Tadiparti");
+	}
+
+}
